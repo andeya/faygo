2d74893149018b47277089cbe1af56bf81fa7a55
henrylee2cn <henrylee_cn@foxmail.com>
2017-01-01 12:00:23 +0800

    update directsql demo

--- samples/directsql/sys_user.sql
+++ samples/directsql/sys_user.sql
@@ -4,7 +4,7 @@ MySQL Data Transfer
 Source Server         : MysqlLocal
 Source Server Version : 50712
 Source Host           : localhost:3306
-Source Database       : bimonline
+Source Database       : thinkgo
 
 Target Server Type    : MYSQL
 Target Server Version : 50712
