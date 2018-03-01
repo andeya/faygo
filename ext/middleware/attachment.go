6fb471c4122a315bb5b7138f2d0c5e622cb7352c
henrylee2cn <henrylee_cn@foxmail.com>
2018-03-01 16:52:37 +0800

    Add attachment middleware

---
+++ ext/middleware/attachment.go
@@ -0,0 +1,47 @@
+// Copyright 2016 HenryLee. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// define common middlewares.
+
+package middleware
+
+import (
+	"strings"
+
+	"github.com/henrylee2cn/faygo"
+)
+
+// NewAttachment has the response content downloaded as an attachment file.
+// Note: if the specifiedExtension is empty, applies to any response content.
+func NewAttachment(specifiedExtension ...string) faygo.HandlerFunc {
+	hash := make(map[string]bool, len(specifiedExtension))
+	for _, s := range specifiedExtension {
+		hash[s] = true
+	}
+	return func(ctx *faygo.Context) error {
+		var isAttachment bool
+		if len(specifiedExtension) == 0 {
+			isAttachment = true
+		} else {
+			p := ctx.Path()
+			if idx := strings.LastIndex(p, "."); idx != -1 {
+				isAttachment = hash[p[idx+1:]]
+			}
+		}
+		if isAttachment {
+			ctx.SetHeader(faygo.HeaderContentDisposition, "attachment")
+		}
+		return nil
+	}
+}
