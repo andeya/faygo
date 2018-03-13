8da60ab13c554a36632b8d9bebce08c380fbe40a
henrylee2cn <henrylee_cn@foxmail.com>
2018-03-13 14:05:59 +0800

    ext/middleware: Update cross_origin

--- ext/middleware/cross_origin.go
+++ ext/middleware/cross_origin.go
@@ -24,9 +24,11 @@ import (
 // Note: The router node should add the OPTIONS method.
 var CrossOrigin faygo.HandlerFunc = func(ctx *faygo.Context) error {
 	ctx.SetHeader(faygo.HeaderAccessControlAllowOrigin, ctx.HeaderParam(faygo.HeaderOrigin))
+	// ctx.SetHeader(faygo.HeaderAccessControlAllowMethods, "POST, GET, OPTIONS, PUT, DELETE")
+	ctx.SetHeader(faygo.HeaderAccessControlAllowMethods, ctx.HeaderParam(faygo.HeaderAccessControlRequestMethod))
+	// ctx.SetHeader(faygo.HeaderAccessControlAllowHeaders, "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	ctx.SetHeader(faygo.HeaderAccessControlAllowHeaders, ctx.HeaderParam(faygo.HeaderAccessControlRequestHeaders))
 	ctx.SetHeader(faygo.HeaderAccessControlAllowCredentials, "true")
-	ctx.SetHeader(faygo.HeaderAccessControlAllowMethods, "POST, GET, OPTIONS, PUT, DELETE")
-	ctx.SetHeader(faygo.HeaderAccessControlAllowHeaders, "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 	ctx.SetHeader(faygo.HeaderAccessControlMaxAge, "172800")
 	if ctx.IsOptions() {
 		ctx.W.WriteHeader(204)
